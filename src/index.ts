fe6b879126b2faac1842525688cff2263544832b
Bruno Polo <brunopolo@poli.ufrj.br>
2024-01-15 15:52:40 -0300

    export useTransformSrc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import DropzoneInputPlugin from './DropzoneInputPlugin';
+import useTransformSrc from './hooks/useTransformSrc';
+
+export {
+    useTransformSrc,
+};
 
 export default DropzoneInputPlugin;
